3ee110dfbc6092116f31c2e1b28081d965a697a8
Guilherme Regis <guilhermeb.regis@gmail.com>
2021-10-13 10:16:57 -0300

    created styles for FormStep1

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -3,10 +3,28 @@ import * as C from './styles';
 import { Theme } from '../../components/Theme'
 
 export const FormStep1 = () => {
+
+    const handleNextStep= () => {
+        
+    };
+
     return (
         <Theme>
             <C.Container>
-                Passo 1
+                <p>Passo 1/3</p>
+                <h1>Vamos começar com o seu nome</h1>
+                <p>Preencha o campo abaixo com o seu nome completo.</p>
+
+                <hr />
+
+                <label>
+                    Seu nome completo
+                    <input
+                        type="text"
+                        autoFocus={true}
+                    />
+                </label>
+                <button onClick={handleNextStep}>Próximo</button>
             </C.Container>
         </Theme>
     );
