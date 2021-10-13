747ada81a2e46d08464e64adf3a9a5615eec9b0e
Guilherme Regis <guilhermeb.regis@gmail.com>
2021-10-13 15:58:49 -0300

    created side bar

--- src/components/Theme/index.tsx
+++ src/components/Theme/index.tsx
@@ -2,13 +2,17 @@ import { ReactNode } from 'react';
 import * as C from './styles';
 
 import { Header } from '../Header';
-import { SidebarItem } from '../SidebarItem'
+import { SidebarItem } from '../SidebarItem';
+import { useForm } from '../../contexts/FormContext'
+
 
 type Props = {
     children: ReactNode;
 }
 
 export const Theme = ({children}: Props) => {
+    const { state } = useForm();
+
     return(
         <C.Container>
             <C.Area>
@@ -21,18 +25,23 @@ export const Theme = ({children}: Props) => {
                             description="Se identifique"
                             icon="profile"
                             path="/"
+                            active={state.currentStep === 1}
                         />
                          <SidebarItem 
                             title="Profissional"
                             description="Seu nível"
                             icon="book"
                             path="/step2"
+                            active={state.currentStep === 2}
+
                         />
                          <SidebarItem 
                             title="Contato"
                             description="Como te achar"
                             icon="mail"
                             path="/step3"
+                            active={state.currentStep === 3}
+
                         />
                         
                     </C.Sidebar>
